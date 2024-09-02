699fea33ffe61463f479c4eefa3fd071b23e59ba
HC <work.cedricjoseph@gmail.com>
2024-09-02 12:18:41 +0200

    refactor: extends the base template, use home as default url. fix: use the templates directory of django instead of REACT_INDEX_DIR. confs: add the context processor to settings

--- LANDING-PAGE-ENGINE/backend/cfehome/settings.py
+++ LANDING-PAGE-ENGINE/backend/cfehome/settings.py
@@ -63,7 +63,7 @@ else:
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [REACT_INDEX_DIR],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -71,6 +71,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "cfehome.context_processor.reactjs_assets_paths",
             ],
         },
     },
